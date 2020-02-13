22606c573fb6966b554a443e9c8a740e4ee8004f
Sheena Puthanpurayil <sheena24789@gmail.com>
2020-02-13 14:23:38 +0200

    adding pytest

---
+++ test_platform_in.py
@@ -0,0 +1,27 @@
+from platform_in import app as flask_app
+import pytest
+
+@pytest.fixture(scope='module')
+def test_client():
+    #flask_app = create_app('flask_test.cfg')
+ 
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = flask_app.test_client()
+ 
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+ 
+    yield testing_client  # this is where the testing happens!
+ 
+    ctx.pop()
+
+def test_home_page(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/')
+    assert response.status_code == 200
